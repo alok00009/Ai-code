699825d1e1237cefc0a05404cc5c4b8610d3bdd1
alok00009 <yy3500212@gmail.com>
2026-04-20 17:45:35 +0530

    Update Firebase config and initialization logic
    Updated Firebase configuration with placeholders for sensitive data and added a check to initialize Firebase only if no apps are already initialized.

--- database.js
+++ database.js
@@ -1,14 +1,16 @@
 const firebaseConfig = {
-    apiKey: "AIzaSy...",
+    apiKey: "YOUR_API_KEY",
     authDomain: "ghhhh-fb825.firebaseapp.com",
     databaseURL: "https://ghhhh-fb825-default-rtdb.firebaseio.com",
     projectId: "ghhhh-fb825",
     storageBucket: "ghhhh-fb825.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
 
 function saveToDatabase(period, prediction, logic) {
